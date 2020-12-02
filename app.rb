77e1c023f16758687a7b5a712150e8462a1bd377
Alicia Weenum <weenum.alicia@gmail.com>
2020-12-01 21:43:38 -0500

    Done.

--- app.rb
+++ app.rb
@@ -6,9 +6,19 @@ class App < Sinatra::Base
     erb :index
   end
 
+
   post '/' do
     text_from_user = params[:user_text]
-
+  
+    @analyzed_text = TextAnalyzer.new(text_from_user)
+  
     erb :results
+
+    if has_text?("Number of Words: 4")
+      true 
+    else
+      false
+    end
+    # use erb tags to display the data stored in the @analyzed_text variable
   end
 end
